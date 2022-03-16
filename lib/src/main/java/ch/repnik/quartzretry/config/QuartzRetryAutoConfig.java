bf24990ed8af25b1eb4ee409a6fde72540b63207
Thomas Repnik <thomas.repnik@gmx.ch>
2022-03-16 17:42:26 +0100

    simplified logic

--- lib/src/main/java/ch/repnik/quartzretry/config/QuartzRetryAutoConfig.java
+++ lib/src/main/java/ch/repnik/quartzretry/config/QuartzRetryAutoConfig.java
@@ -1,14 +1,8 @@
 package ch.repnik.quartzretry.config;
 
-import org.quartz.Trigger;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.quartz.SchedulerFactoryBean;
-import org.springframework.scheduling.quartz.SpringBeanJobFactory;
 
 import javax.sql.DataSource;
 
@@ -16,29 +10,5 @@ import javax.sql.DataSource;
 @ConditionalOnBean(DataSource.class)
 @EnableConfigurationProperties(QuartzRetryProperties.class)
 public class QuartzRetryAutoConfig {
-
-    @Bean
-    @ConditionalOnMissingBean
-    public SchedulerFactoryBean scheduler(DataSource dataSource, SpringBeanJobFactory jobFactory, Trigger... triggers) {
-        final SchedulerFactoryBean schedulerFactory = new SchedulerFactoryBean();
-
-        schedulerFactory.setOverwriteExistingJobs(false);
-        schedulerFactory.setAutoStartup(true);
-        schedulerFactory.setDataSource(dataSource);
-        schedulerFactory.setJobFactory(jobFactory);
-        schedulerFactory.setWaitForJobsToCompleteOnShutdown(true);
-        if (triggers != null && triggers.length > 0) {
-            schedulerFactory.setTriggers(triggers);
-        }
-        return schedulerFactory;
-    }
-
-    @Bean
-    @ConditionalOnMissingBean
-    public SpringBeanJobFactory springBeanJobFactory(ApplicationContext context) {
-        final SpringBeanJobFactory jobFactory = new SpringBeanJobFactory();
-        jobFactory.setApplicationContext(context);
-        return jobFactory;
-    }
 
 }
--- lib/src/test/java/ch/repnik/quartzretry/ApplicationIntegrationTest.java
+++ lib/src/test/java/ch/repnik/quartzretry/ApplicationIntegrationTest.java
@@ -1,14 +1,12 @@
 package ch.repnik.quartzretry;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationIT {
+class ApplicationIntegrationTest {
 
     @Test
     void load_startupSuccessful() {
-        Assertions.assertTrue(true);
     }
 }
